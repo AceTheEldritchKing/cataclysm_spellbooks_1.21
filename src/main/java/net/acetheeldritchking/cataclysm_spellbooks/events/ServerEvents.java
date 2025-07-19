cd10bf0ad95965613221713fec72bbc5d8e12fe8
SojaIntegral <45701855+SojaIntegral@users.noreply.github.com>
2025-07-19 12:42:36 -0300

    Fixed bosses resistances + added config

--- src/main/java/net/acetheeldritchking/cataclysm_spellbooks/events/ServerEvents.java
+++ src/main/java/net/acetheeldritchking/cataclysm_spellbooks/events/ServerEvents.java
@@ -42,8 +42,6 @@ import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
 import net.neoforged.neoforge.event.entity.living.*;
 import net.neoforged.neoforge.event.tick.EntityTickEvent;
 
-import java.io.StringReader;
-
 @SuppressWarnings("unused")
 @EventBusSubscriber
 public class ServerEvents {
@@ -187,45 +185,45 @@ public class ServerEvents {
 
             if (mob.getType() == ModEntities.IGNIS.get()) {
                 // Ignis takes extra abyssal damage, and less fire damage
-                addIfNonNull(mob, CSAttributeRegistry.ABYSSAL_MAGIC_RESIST, -0.5, "cats_subtract");
-                addIfNonNull(mob, AttributeRegistry.FIRE_MAGIC_RESIST, 0.5, "cats_add");
+                addIfNonNull(mob, CSAttributeRegistry.ABYSSAL_MAGIC_RESIST, 0.5);
+                addIfNonNull(mob, AttributeRegistry.FIRE_MAGIC_RESIST, 1.5);
             }
             if (mob.getType() == ModEntities.THE_LEVIATHAN.get()) {
                 // Leviathan takes extra lightning damage, and less abyssal damage
-                addIfNonNull(mob, AttributeRegistry.LIGHTNING_MAGIC_RESIST, -0.5, "cats_subtract");
-                addIfNonNull(mob, CSAttributeRegistry.ABYSSAL_MAGIC_RESIST, 0.5, "cats_add");
+                addIfNonNull(mob, AttributeRegistry.LIGHTNING_MAGIC_RESIST, 0.5);
+                addIfNonNull(mob, CSAttributeRegistry.ABYSSAL_MAGIC_RESIST, 1.5);
             }
             if (mob.getType() == ModEntities.ENDER_GUARDIAN.get()) {
                 // Ender Guardian takes extra ice damage, and less ender damage
-                addIfNonNull(mob, AttributeRegistry.ICE_MAGIC_RESIST, -0.5, "cats_subtract");
-                addIfNonNull(mob, AttributeRegistry.ENDER_MAGIC_RESIST, 0.5, "cats_add");
+                addIfNonNull(mob, AttributeRegistry.ICE_MAGIC_RESIST, 0.5);
+                addIfNonNull(mob, AttributeRegistry.ENDER_MAGIC_RESIST, 1.5);
             }
             if (mob.getType() == ModEntities.THE_HARBINGER.get()) {
                 // Harbinger takes extra lightning damage, and less blood damage
-                addIfNonNull(mob, AttributeRegistry.LIGHTNING_MAGIC_RESIST, -0.5, "cats_subtract");
-                addIfNonNull(mob, AttributeRegistry.BLOOD_MAGIC_RESIST, 0.5, "cats_add");
+                addIfNonNull(mob, AttributeRegistry.LIGHTNING_MAGIC_RESIST, 0.5);
+                addIfNonNull(mob, AttributeRegistry.BLOOD_MAGIC_RESIST, 1.5);
             }
             if (mob.getType() == ModEntities.ANCIENT_REMNANT.get()) {
                 // Ancient Remnant takes extra holy damage, and less fire damage(?)
-                addIfNonNull(mob, AttributeRegistry.HOLY_MAGIC_RESIST, -0.5, "cats_subtract");
-                addIfNonNull(mob, AttributeRegistry.FIRE_MAGIC_RESIST, 0.5, "cats_add");
+                addIfNonNull(mob, AttributeRegistry.HOLY_MAGIC_RESIST, 0.5);
+                addIfNonNull(mob, AttributeRegistry.FIRE_MAGIC_RESIST, 1.5);
             }
             if (mob.getType() == ModEntities.NETHERITE_MONSTROSITY.get()) {
                 // Netherite Monstrosity takes extra ice damage, and less fire damage
-                addIfNonNull(mob, AttributeRegistry.ICE_MAGIC_RESIST, -0.5, "cats_subtract");
-                addIfNonNull(mob, AttributeRegistry.FIRE_MAGIC_RESIST, 0.5, "cats_add");
+                addIfNonNull(mob, AttributeRegistry.ICE_MAGIC_RESIST, 0.5);
+                addIfNonNull(mob, AttributeRegistry.FIRE_MAGIC_RESIST, 1.5);
             }
             if (mob.getType() == ModEntities.MALEDICTUS.get()) {
                 // Maledictus takes extra eldritch damage, and less ice damage
-                addIfNonNull(mob, AttributeRegistry.ELDRITCH_MAGIC_RESIST, -0.5, "cats_subtract");
-                addIfNonNull(mob, AttributeRegistry.ICE_MAGIC_RESIST, 0.5, "cats_add");
+                addIfNonNull(mob, AttributeRegistry.ELDRITCH_MAGIC_RESIST, 0.5);
+                addIfNonNull(mob, AttributeRegistry.ICE_MAGIC_RESIST, 1.5);
             }
             /* Added Scylla tag for future use
             if (mob.getType() == ModEntities.SCYLLA.get())
             {
                 // Scylla takes extra nature damage, and less ice damage
-                addIfNonNull(mob, AttributeRegistry.NATURE_MAGIC_RESIST, -0.5, "cats_subtract");
-                addIfNonNull(mob, AttributeRegistry.LIGHTNING_MAGIC_RESIST, 0.5, "cats_add");
+                addIfNonNull(mob, AttributeRegistry.NATURE_MAGIC_RESIST, 0.5);
+                addIfNonNull(mob, AttributeRegistry.LIGHTNING_MAGIC_RESIST, 1.5);
             }
             */
         }
@@ -245,21 +243,6 @@ public class ServerEvents {
         }
     }
 
-    // New event system for entities
-    // It adds attributes on event trigger, instead of setting them
-    private static void addIfNonNull(LivingEntity entity, Holder<Attribute> attribute, double value, String idName) {
-        var instance = entity.getAttributes().getInstance(attribute);
-        if (instance == null) return;
-
-        ResourceLocation id = ResourceLocation.fromNamespaceAndPath("cataclysm_spellbooks", idName);
-
-        // Avoid duplicates
-        if (instance.getModifiers().stream().noneMatch(mod -> mod.id().equals(id))) {
-            instance.addPermanentModifier(new AttributeModifier(id, value, AttributeModifier.Operation.ADD_VALUE));
-        }
-    }
-
-    /* Old event system for entities
     // Using same code from ISS for dealing with mob attributes, please forgive me
     private static void addIfNonNull(LivingEntity entity, Holder<Attribute> attribute, double value)
     {
@@ -269,7 +252,6 @@ public class ServerEvents {
             instance.setBaseValue(value);
         }
     }
-     */
 
     // Modify Spell Event
     @SubscribeEvent
